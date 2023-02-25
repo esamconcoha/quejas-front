4cf039a7c9f99face2330cd6471521c83f95f113
Juarezgump655 <ajuarezcastellanos45@gmail.com>
2023-02-25 07:46:29 -0600

    Mejoras de component home y creacion de registrars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { MiPrestamitoRegistrarseComponent } from './componentes/Usuario-Externo/Mi-prestamito-registrarse/Mi-prestamito-registrarse.component';
 
 const routes: Routes = [
-{path:'menu-principal',component:HomeComponent},
-{path:'',redirectTo:'menu-principal',pathMatch:'full'},
-{path:'login',component:LoginComponent}
+  {path : 'menu-principal', component:HomeComponent},
+  {path:'',redirectTo:'menu-principal',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'registrarse-usuario',component:MiPrestamitoRegistrarseComponent}
+
 ];
 
 @NgModule({
---
+++ src/app/componentes/Usuario-Externo/Mi-prestamito-registrarse/Mi-prestamito-registrarse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Mi-prestamito-registrarse',
+  templateUrl: './Mi-prestamito-registrarse.component.html',
+  styleUrls: ['./Mi-prestamito-registrarse.component.css']
+})
+export class MiPrestamitoRegistrarseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
